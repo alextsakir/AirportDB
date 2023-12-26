4520886259801c0346c4554f2e8789a9ebfed4d6
user <alexandros.tsakiridis2@gmail.com>
2023-12-26 14:37:26 +0200

    simplified
    Signed-off-by: user <alexandros.tsakiridis2@gmail.com>

--- scripts/schedules.py
+++ scripts/schedules.py
@@ -1,11 +1,11 @@
 from assets import *
 
 QUERY = ("select code, A2.IATA, A.IATA, departure, arrival, days from Schedule "
-         "join main.Airport A on A.flight_id = Schedule.end "
-         "join main.Airport A2 on A2.flight_id = Schedule.start")
+         "join main.Airport A on A.id = Schedule.to_airport "
+         "join main.Airport A2 on A2.id = Schedule.from_airport")
 
 data = database(QUERY).fetchall()
 for schedule in data:
     airline = database(f"select name from Airline where designator = '{schedule[0][:2]}'").fetchone()[0]
-    print(schedule[0], airline, schedule[1], schedule[2], schedule[3], schedule[4], end="    ")
-    print(" ".join([member.value[1] for member in Day.code_to_days(schedule[5])]))
+    print(schedule[0], airline, schedule[1], schedule[2], schedule[3], schedule[4], sep="    ", end="    ")
+    print("  ".join([member.value[1] for member in Day.code_to_days(schedule[5])]))
